bdbc9c5cc0143102c2e9746f4093afbf0077de92
Ahmad Karimi <ahmadkarimi1991@gmail.com>
2021-08-19 21:24:03 +0430

    Create required sass files and header section for mobile

---
+++ resources/views/discount.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fa" dir="rtl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    <title>تخفیف ویژه‌ی محصولات ابر آروان</title>
+</head>
+
+<body>
+    <section class="discount-header">
+        <div class="container">
+            <div class="d-lg-flex">
+                <img src="{{ asset('images/header.png') }}" alt="تخفیف ویژه‌ی محصولات ابر آروان" class="discount-header__image">
+
+                <div>
+                    <h1 class="discount-header__title">تخفیف ویژه‌ی محصولات ابر آروان</h1>
+                    <div class="discount-header__text">از ۵۰ تا ۸۰ درصد تخفیف تابستانی استفاده از فضای ذخیره‌سازی ابری، پلتفرم ویدیو و شبکه‌ی توزیع محتوا برای استارتاپ‌ها و کسب‌وکارهای آنلاین</div>
+                    <a href="#" class="discount-header__button">ثبت درخواست</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+
+</html>
