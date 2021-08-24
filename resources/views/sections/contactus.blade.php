66bfe7ccdbf406b8f1cbe6941387bbde0fdb7a3d
Ahmad Karimi <ahmadkarimi1991@gmail.com>
2021-08-25 02:46:33 +0430

    Create contact us section

---
+++ resources/views/sections/contactus.blade.php
@@ -0,0 +1,100 @@
+<section class="discount-contactus">
+    <div class="container">
+        <div class="discount-contactus__text">برای استفاده از این تخفیف ویژه، اطلاعات خود را وارد کنید تا با شما تماس بگیریم</div>
+        <div class="discount-contactus__date">فقط تا ۱۵ شهریور ۱۳۹۹</div>
+
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="discount-contactus__form-group mb-3">
+                    <input type="text" name="name" id="name" class="discount-contactus__form-control" placeholder="نام و نام خانوادگی">
+                    <label for="name" class="discount-contactus__label">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="14.735" height="16.22" viewBox="0 0 14.735 16.22">
+                            <path id="Path_87113" data-name="Path 87113" d="M16.735,19.776V18.184A3.184,3.184,0,0,0,13.551,15H7.184A3.184,3.184,0,0,0,4,18.184v1.592" transform="translate(-3 -4.556)" fill="none" stroke="#009191" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" opacity="0.5"/>
+                            <ellipse id="Ellipse_1103" data-name="Ellipse 1103" cx="2.929" cy="2.929" rx="2.929" ry="2.929" transform="translate(4.661 1)" fill="none" stroke="#009191" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" opacity="0.5"/>
+                        </svg>
+                    </label>
+                </div>
+            </div>
+
+            <div class="col-lg-6">
+                <div class="discount-contactus__form-group mb-3">
+                    <input type="email" name="email" id="email" class="discount-contactus__form-control discount-contactus__form-control--ltr" placeholder="ایمیل">
+                    <label for="email" class="discount-contactus__label">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20.378" height="16.074" viewBox="0 0 20.378 16.074">
+                            <g id="mail" transform="translate(1.393 1)" opacity="0.5">
+                                <path id="Path_5701" data-name="Path 5701" d="M3.759,4H17.834a1.764,1.764,0,0,1,1.759,1.759V16.315a1.764,1.764,0,0,1-1.759,1.759H3.759A1.764,1.764,0,0,1,2,16.315V5.759A1.764,1.764,0,0,1,3.759,4Z" transform="translate(-2 -4)" fill="none" stroke="#009191" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+                                <path id="Path_5702" data-name="Path 5702" d="M19.593,6l-8.8,6.158L2,6" transform="translate(-2 -4.241)" fill="none" stroke="#009191" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+                            </g>
+                        </svg>
+                    </label>
+                </div>
+            </div>
+
+            <div class="col-lg-6">
+                <div class="discount-contactus__form-group mb-3">
+                    <input type="tel" name="mobile" id="mobile" class="discount-contactus__form-control discount-contactus__form-control--ltr" placeholder="موبایل">
+                    <label for="mobile" class="discount-contactus__label">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="18" viewBox="0 0 14 18">
+                            <g id="smartphone" transform="translate(-3.6 -1)" opacity="0.5">
+                                <rect id="Rectangle_3019" data-name="Rectangle 3019" width="12" height="16" rx="2" transform="translate(4.6 2)" fill="none" stroke="#009191" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+                                <line id="Line_892" data-name="Line 892" transform="translate(10.6 15)" fill="none" stroke="#009191" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+                            </g>
+                        </svg>
+                    </label>
+                </div>
+            </div>
+
+            <div class="col-lg-6">
+                <div class="discount-contactus__form-group mb-3">
+                    <input type="text" name="company" id="company" class="discount-contactus__form-control" placeholder="نام شرکت">
+                    <label for="company" class="discount-contactus__label">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21">
+                            <g id="target" transform="translate(-0.842 -1)">
+                                <circle id="Ellipse_1105" data-name="Ellipse 1105" cx="9.5" cy="9.5" r="9.5" transform="translate(1.842 2)" fill="none" stroke="#76c5c7" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+                                <circle id="Ellipse_1106" data-name="Ellipse 1106" cx="6.5" cy="6.5" r="6.5" transform="translate(4.842 5)" fill="none" stroke="#76c5c7" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+                                <circle id="Ellipse_1107" data-name="Ellipse 1107" cx="2.5" cy="2.5" r="2.5" transform="translate(8.842 9)" fill="none" stroke="#76c5c7" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+                            </g>
+                        </svg>
+                    </label>
+                </div>
+            </div>
+
+            <div class="col-lg-12">
+                <div class="discount-contactus__packages">
+                    <div class="discount-contactus__packages-label">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
+                            <g id="grid" transform="translate(-2.196 -2.196)">
+                                <rect id="Rectangle_3245" data-name="Rectangle 3245" width="6" height="6" transform="translate(3.196 3.196)" fill="none" stroke="#76c5c7" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+                                <rect id="Rectangle_3246" data-name="Rectangle 3246" width="6" height="6" transform="translate(13.196 3.196)" fill="none" stroke="#76c5c7" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+                                <rect id="Rectangle_3247" data-name="Rectangle 3247" width="6" height="6" transform="translate(13.196 13.196)" fill="none" stroke="#76c5c7" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+                                <rect id="Rectangle_3248" data-name="Rectangle 3248" width="6" height="6" transform="translate(3.196 13.196)" fill="none" stroke="#76c5c7" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+                            </g>
+                        </svg>
+                        <span>بسته تخفیف انتخابی</span>
+                    </div>
+
+                    <div class="discount-contactus__checkbox-wrapper">
+                        <label class="discount-contactus__checkbox-item">
+                            <input type="checkbox" name="cdn" id="cdn" class="discount-contactus__checkbox">
+                            <span>CDN بی‌نهایت</span>
+                        </label>
+
+                        <label class="discount-contactus__checkbox-item">
+                            <input type="checkbox" name="storage" id="storage" class="discount-contactus__checkbox">
+                            <span>فضای ذخیره‌سازی ابری</span>
+                        </label>
+
+                        <label class="discount-contactus__checkbox-item">
+                            <input type="checkbox" name="vod" id="vod" class="discount-contactus__checkbox">
+                            <span>پلتفرم ویدیویی/صوتی</span>
+                        </label>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12">
+                <button class="discount-contactus__button">ثبت درخواست</button>
+            </div>
+        </div>
+    </div>
+</section>
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,5 +13,5 @@ require('laravel-mix-purgecss');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
-    .purgeCss();
+    .sass('resources/sass/app.scss', 'public/css');
+    // .purgeCss();
