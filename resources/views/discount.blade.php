fbb1ffdb26ade91024ef53bf6872d9d27227bdba
Ahmad Karimi <ahmadkarimi1991@gmail.com>
2021-08-24 05:34:34 +0430

    Create a discount feature item

--- resources/views/discount.blade.php
+++ resources/views/discount.blade.php
@@ -12,19 +12,8 @@
 </head>
 
 <body>
-    <section class="discount-header">
-        <div class="container">
-            <div class="d-lg-flex">
-                <img src="{{ asset('images/header.png') }}" alt="تخفیف ویژه‌ی محصولات ابر آروان" class="discount-header__image">
-
-                <div>
-                    <h1 class="discount-header__title">تخفیف ویژه‌ی محصولات ابر آروان</h1>
-                    <div class="discount-header__text">از ۵۰ تا ۸۰ درصد تخفیف تابستانی استفاده از فضای ذخیره‌سازی ابری، پلتفرم ویدیو و شبکه‌ی توزیع محتوا برای استارتاپ‌ها و کسب‌وکارهای آنلاین</div>
-                    <a href="#" class="discount-header__button">ثبت درخواست</a>
-                </div>
-            </div>
-        </div>
-    </section>
+    @include('sections.header')
+    @include('sections.features')
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
     <script src="{{ asset('js/app.js') }}"></script>
